3dc578bf98c34dd91fda44e2bccef08bb33bbced
DKosenkov <89424643+DKosenkov@users.noreply.github.com>
2022-04-19 16:55:11 -0400

    Create tests.sh

---
+++ src/tests/tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Run the script with: '/bin/bash -x' for debugging
+#
+cd ..
+#rem save results of tests to the "tests" directory
+conda run -n pyfrec pytest test_pyfrec.py 1> ./tests/tests.log 2>&1
+cd ./tests
