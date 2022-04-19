553962ee3d3d147683989825e3b0f2625ec7fc41
DKosenkov <89424643+DKosenkov@users.noreply.github.com>
2022-04-19 16:02:35 -0400

    Update run.bat

--- samples/02_bodipy_trz/Forster/run.bat
+++ samples/02_bodipy_trz/Forster/run.bat
@@ -1,8 +1,6 @@
 rem Sample BODIPY-TRZ calculations: electronic coupings and Forster energy transfer modeling
 set "PYTHON_CMD=python -u"
 set "PYFREC_CMD=.\..\..\..\src\pyfrec.py -f"
-conda activate pyfrec
 %PYTHON_CMD% %PYFREC_CMD% .\bodipy_a_ac.ini 1> .\bodipy_a_ac.log 2>&1
 %PYTHON_CMD% %PYFREC_CMD% .\bodipy_b_ac.ini 1> .\bodipy_b_ac.log 2>&1
 %PYTHON_CMD% %PYFREC_CMD% .\bodipy_c_ac.ini 1> .\bodipy_c_ac.log 2>&1
-conda deactivate
