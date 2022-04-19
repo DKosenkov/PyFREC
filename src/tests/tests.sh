70807e00b5f0876cb63fb57358bc580ec5f7de0a
DKosenkov <89424643+DKosenkov@users.noreply.github.com>
2022-04-19 16:53:09 -0400

    Delete tests.sh

--- src/tests/tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-# Run the script with: '/bin/bash -x' for debugging
-#
-cd ..
-#rem save results of tests to the "tests" directory
-conda run -n pyfrec pytest tests.py 1> ./tests/tests.log 2>&1
-cd ./tests
-
-
