fab87b9787496c9d510dd2508434512f2d6de120
DKosenkov <89424643+DKosenkov@users.noreply.github.com>
2022-04-19 16:36:56 -0400

    Update tests.sh

--- src/tests/tests.sh
+++ src/tests/tests.sh
@@ -1,13 +1,9 @@
 #!/bin/bash 
 # Run the script with: '/bin/bash -x' for debugging
 #
-cd .\..\src
+cd ./../src
 #rem save results of tests to the "tests" directory
-#%PYTHON_CMD% %PYTEST_MY_CMD% 1> .\..\tests\tests.log 2>&1
-#conda run -n pyfrec pytest tests.py --junitxml result_Tests.xml
 conda run -n pyfrec pytest tests.py 1> .\..\tests\tests.log 2>&1
-#call .\..\bin\pyfrec-dea.bat
-#rem conda install -c anaconda pytest
-cd .\tests
+cd ./tests
 
 
