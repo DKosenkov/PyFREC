a578f649b1309f1e23412e3695cce2b9e48fda3d
DKosenkov <89424643+DKosenkov@users.noreply.github.com>
2022-04-26 17:48:01 -0400

    Update tests.sh

--- src/tests/tests.sh
+++ src/tests/tests.sh
@@ -1,11 +1,3 @@
-#!/bin/bash 
-#conda install -c anaconda pytest
-#location of the tested codes (NOT data files ...)
-cd ..
-# save results of tests to the "tests" directory
-conda run -n pyfrec pytest test_pyfrec.py 1> ./tests/tests.log 2>&1
-cd tests
-
 #!/bin/bash
 # Run the script with: '/bin/bash -x' for debugging
 #
